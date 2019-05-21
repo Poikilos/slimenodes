8f078c87023d0565f023ffe62210843b3cad5142
poikilos <7557867+poikilos@users.noreply.github.com>
2019-05-20 21:25:01 -0400

    fix depends to allow mod do load

--- buckets.lua
+++ buckets.lua
@@ -18,70 +18,73 @@
 --                  Needed to avoid creating holes in sloping rivers.
 -- This function can be called from any mod (that depends on bucket).
 
-bucket.register_liquid(
-	"slimenodes:slime_liquid_cyan",
-	"slimenodes:slime_liquid_cyan",
-	"slimenodes:bucket_slime_liquid_cyan",
-	"slimenodes_bucket_slime_liquid_cyan.png",
-	"Liquid Cyan Slime Bucket"
-)
-bucket.register_liquid(
-	"slimenodes:slime_solid_cyan",
-	"slimenodes:slime_solid_cyan",
-	"slimenodes:bucket_slime_solid_cyan",
-	"slimenodes_bucket_slime_solid_cyan.png",
-	"Solid Cyan Slime Bucket"
-)
+if rawget(_G, "bucket") and bucket.register_liquid then
 
-bucket.register_liquid(
-	"slimenodes:slime_liquid_green",
-	"slimenodes:slime_liquid_green",
-	"slimenodes:bucket_slime_liquid_green",
-	"slimenodes_bucket_slime_liquid_green.png",
-	"Liquid Green Slime Bucket"
-)
-bucket.register_liquid(
-	"slimenodes:slime_solid_green",
-	"slimenodes:slime_solid_green",
-	"slimenodes:bucket_slime_solid_green",
-	"slimenodes_bucket_slime_solid_green.png",
-	"Solid Green Slime Bucket"
-)
+	bucket.register_liquid(
+		"slimenodes:slime_liquid_cyan",
+		"slimenodes:slime_liquid_cyan",
+		"slimenodes:bucket_slime_liquid_cyan",
+		"slimenodes_bucket_slime_liquid_cyan.png",
+		"Liquid Cyan Slime Bucket"
+	)
+	bucket.register_liquid(
+		"slimenodes:slime_solid_cyan",
+		"slimenodes:slime_solid_cyan",
+		"slimenodes:bucket_slime_solid_cyan",
+		"slimenodes_bucket_slime_solid_cyan.png",
+		"Solid Cyan Slime Bucket"
+	)
 
-bucket.register_liquid(
-	"slimenodes:slime_liquid_orange",
-	"slimenodes:slime_liquid_orange",
-	"slimenodes:bucket_slime_liquid_orange",
-	"slimenodes_bucket_slime_liquid_orange.png",
-	"Liquid Orange Slime Bucket"
-)
-bucket.register_liquid(
-	"slimenodes:slime_solid_orange",
-	"slimenodes:slime_solid_orange",
-	"slimenodes:bucket_slime_solid_orange",
-	"slimenodes_bucket_slime_solid_orange.png",
-	"Solid Orange Slime Bucket"
-)
+	bucket.register_liquid(
+		"slimenodes:slime_liquid_green",
+		"slimenodes:slime_liquid_green",
+		"slimenodes:bucket_slime_liquid_green",
+		"slimenodes_bucket_slime_liquid_green.png",
+		"Liquid Green Slime Bucket"
+	)
+	bucket.register_liquid(
+		"slimenodes:slime_solid_green",
+		"slimenodes:slime_solid_green",
+		"slimenodes:bucket_slime_solid_green",
+		"slimenodes_bucket_slime_solid_green.png",
+		"Solid Green Slime Bucket"
+	)
 
-bucket.register_liquid(
-	"slimenodes:slime_liquid_violet",
-	"slimenodes:slime_liquid_violet",
-	"slimenodes:bucket_slime_liquid_violet",
-	"slimenodes_bucket_slime_liquid_violet.png",
-	"Liquid Violet Slime Bucket"
-)
-bucket.register_liquid(
-	"slimenodes:slime_solid_violet",
-	"slimenodes:slime_solid_violet",
-	"slimenodes:bucket_slime_solid_violet",
-	"slimenodes_bucket_slime_solid_violet.png",
-	"Solid Violet Slime Bucket"
-)
+	bucket.register_liquid(
+		"slimenodes:slime_liquid_orange",
+		"slimenodes:slime_liquid_orange",
+		"slimenodes:bucket_slime_liquid_orange",
+		"slimenodes_bucket_slime_liquid_orange.png",
+		"Liquid Orange Slime Bucket"
+	)
+	bucket.register_liquid(
+		"slimenodes:slime_solid_orange",
+		"slimenodes:slime_solid_orange",
+		"slimenodes:bucket_slime_solid_orange",
+		"slimenodes_bucket_slime_solid_orange.png",
+		"Solid Orange Slime Bucket"
+	)
 
--- minetest.register_craft({
-	-- type = "fuel",
-	-- recipe = "bucket:bucket_lava",
-	-- burntime = 60,
-	-- replacements = {{"bucket:bucket_lava", "bucket:bucket_empty"}},
--- })
+	bucket.register_liquid(
+		"slimenodes:slime_liquid_violet",
+		"slimenodes:slime_liquid_violet",
+		"slimenodes:bucket_slime_liquid_violet",
+		"slimenodes_bucket_slime_liquid_violet.png",
+		"Liquid Violet Slime Bucket"
+	)
+	bucket.register_liquid(
+		"slimenodes:slime_solid_violet",
+		"slimenodes:slime_solid_violet",
+		"slimenodes:bucket_slime_solid_violet",
+		"slimenodes_bucket_slime_solid_violet.png",
+		"Solid Violet Slime Bucket"
+	)
 
+	-- minetest.register_craft({
+		-- type = "fuel",
+		-- recipe = "bucket:bucket_lava",
+		-- burntime = 60,
+		-- replacements = {{"bucket:bucket_lava", "bucket:bucket_empty"}},
+	-- })
+
+end
